38785d8be3040a800bb555d067cb566d81350b03
Mildred Awuor <otieno.awuor.m@gmail.com>
2020-03-20 11:29:10 +0300

    added name attribute to player

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 class Player:
-    def __init__(self, starting_room):
+    def __init__(self,name, starting_room):
+        self.name = name
         self.current_room = starting_room
     def travel(self, direction, show_rooms = False):
         next_room = self.current_room.get_room_in_direction(direction)
@@ -8,4 +9,4 @@ class Player:
             if (show_rooms):
                 next_room.print_room_description(self)
         else:
-            print("You cannot move in that direction.")
+            print("You can
